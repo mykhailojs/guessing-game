d0aad69e4c7fff8de399491eb3b15507aadfc653
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-25 14:10:47 +0300

    feat: finish app

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.index = null;
+    }
+
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
+
 
     guess() {
-
-    }
+        return  this.index = Math.ceil((this.min + this.max) / 2);
+}
 
     lower() {
+        this.max = this.index;
 
     }
 
     greater() {
+        this.min = this.index;
 
     }
 }
